45ec9bd7214f527d811773976cb1488f1346adc9
PXR <pxr2709@gmail.com>
2024-12-02 17:58:17 +0700

    cleanup

--- src/main/java/lms/Main.java
+++ src/main/java/lms/Main.java
@@ -2,19 +2,14 @@ package lms;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import lms.models.*;
 import lms.views.ScreenManager;
 
 public class Main extends Application {
-    private final Library library = Library.getInstance();
-    
     @Override
     public void start(Stage primaryStage) {
         ScreenManager.getInstance().setPrimaryStage(primaryStage);
         ScreenManager.getInstance().showLoginScreen();
     }
-    
-
 
     public static void main(String[] args) {
         launch(args);
